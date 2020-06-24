856f3df72b73f18312c114c9e812d36542515bac
Cedrick TAGNE <cedrick.tagne@propellerhead.co.nz>
2020-06-24 17:38:22 +0100

    deleted src to get new one from develop branch

--- src/app/shared/entites/InterviewEvaluationItem.ts
+++
@@ -1,5 +0,0 @@
-export class InterviewEvaluationItem{
-    private title:  string;
-    private element: string;
-    private evaluation?: number;
-}
--- src/app/shared/entites/preinterview.ts
+++
@@ -1,9 +0,0 @@
-export class Preinterview {
-  skillname:string;
-  description:string;
-<<<<<<< HEAD
-=======
-  overallmarks: number;
-  userMarks: number;
->>>>>>> b5446b2a5deb6a99c0106b8227a23d0ad7d05dbe
-}
